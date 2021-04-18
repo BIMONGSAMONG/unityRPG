1dc6a693b7e9124b93fcb66f0598ff5b4b1c35df
BIMONGSAMONG <67625800+BIMONGSAMONG@users.noreply.github.com>
2021-04-19 01:42:55 +0900

    랜덤 위치 몬스터 생성 추가

---
+++ rpg/Assets/Scripts/GenerateEnemies.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public GameObject[] enemy = new GameObject[2];
+
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+    void Start()
+    {
+        StartCoroutine(MushroomEnemyDrop());
+    }
+
+
+    IEnumerator MushroomEnemyDrop()
+    {
+        while (enemyCount < 5)
+        {
+            xPos = UnityEngine.Random.Range(5, 15);
+            zPos = UnityEngine.Random.Range(11, 20);
+
+            Instantiate(enemy[0], new Vector3(xPos, 0, zPos), Quaternion.identity);
+
+            yield return new WaitForSeconds(0.1f);
+            enemyCount++;
+        }
+    }
+}
